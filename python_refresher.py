6e454a5f32f193666a1a26fcda56b56a565a9f36
Zágon Kristóf <tofi.zagon@gmail.com>
2020-07-20 22:06:19 +0200

    Create python_refresher.py
    practice python

---
+++ python_refresher.py
@@ -0,0 +1,40 @@
+"""name = "Kristóf"
+age = 30
+
+def hello(name="Anybody", age=0):
+    print("Hello {} you are {} years old.".format(name, age))
+
+hello(name, 28)"""
+
+
+
+"""def tripleprint(string):
+    print(string*3)
+
+tripleprint("hello")"""
+
+
+
+"""shoes = ["Spizikes", "Air Force 1", "Curry 2", "Melo 5"]"""
+
+
+
+"""for x in range(1,99):
+    print(x)"""
+
+
+
+"""
+age = 0
+
+while age <= 18:
+    print(age)
+    age+=1
+"""
+
+
+
+numbers = [76, 83, 16, 69, 52, 78, 10, 77, 45, 52, 32, 17, 58, 54, 79, 72, 55, 50, 81, 74, 45, 33, 38, 10, 40, 44, 70, 81, 79, 28, 83, 41, 14, 16, 27, 38, 20, 84, 24, 50, 59, 71, 1, 13, 56, 91, 29, 54, 65, 23, 60, 57, 13, 39, 58, 94, 94, 42, 46, 58, 59, 29, 69, 60, 83, 9, 83, 5, 64, 70, 55, 89, 67, 89, 70, 8, 90, 17, 48, 17, 94, 18, 98, 72, 96, 26, 13, 7, 58, 67, 38, 48, 43, 98, 65, 8, 74, 44, 92]
+for number in numbers:
+    if number > 90:
+        print(number)
